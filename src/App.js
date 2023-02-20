153065b08aa8836bb37f1b8169dc95193d926ff1
Aditya <adityadhayagude03@gmail.com~>
2023-02-20 14:21:18 +0530

    this shows using React.createElement syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+// Adding elements without using JSX
+
+import React from 'react'
+
 function App()
 {
-    return(
-        <>
-            <h1>My App</h1>
-            <p>Hello</p>
-        </>
-    )
+   return React.createElement
+   (
+        'div',
+        {className:'container'},
+        React.createElement('h1',{},'My App')
+   )
 }
+
+// So it's basically React.createElemenet(element_type,class_name,elemennt content,loop )
 
 export default App
