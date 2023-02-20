f645cfb37d08c939e2e9fea741cb8ea9280e4a1f
Aditya <adityadhayagude03@gmail.com~>
2023-02-20 14:01:03 +0530

    Wrapping direct HTML code in React with div

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 function App()
 {
-    return <h1>Hello From the app component</h1>
+    return(
+        <div>
+            <h1>My App</h1>
+            <p>Hello</p>
+        </div>
+    )
 }
 
 export default App
