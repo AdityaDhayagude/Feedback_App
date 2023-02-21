1493b0a1958d79dc7962d89a8d83ad7df7b8e065
Aditya <adityadhayagude03@gmail.com~>
2023-02-21 13:42:57 +0530

    Passing in props in order to make CSS work

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import Header from "./components/Header"
 
 function App()
 {
-    // return(
-    //     <>
-    //     <Header text='Hello World'/>
-    //     <div className="container">
-    //         <h1>My App</h1>
-    //     </div>
-    //     </>
-        
-    // )
+
 
     return(
         <>
         {/* //// This is how u pass on the component to the manin app for rendering and hencforth u can pass props which are meant to be caught by components */}
-        <Header text='Feedback-UI'/>  
+        <Header bgColor='red'textColor='blue'/>  
 
 
         
