34e3093c89fb5fd0080867feaf5c913d5d263682
Aditya <adityadhayagude03@gmail.com~>
2023-02-21 14:35:50 +0530

    Making of Feedback Item  & using funtion to make it dynamic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import FeedbackItem from "./components/FeedbackItem"
 import Header from "./components/Header"
 
 function App()
@@ -7,12 +8,12 @@ function App()
     return(
         <>
         {/* //// This is how u pass on the component to the manin app for rendering and hencforth u can pass props which are meant to be caught by components */}
-        <Header bgColor='red'textColor='blue'/>  
+        <Header />  
 
 
         
         <div className="container">
-            <h1>My App</h1>
+            <FeedbackItem />
         </div>
         </>
         
