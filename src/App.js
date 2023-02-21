f9e341eba905315683a5473aaa56a0ebe66196d0
Aditya <adityadhayagude03@gmail.com~>
2023-02-21 15:58:45 +0530

    Managing Global State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import FeedbackItem from "./components/FeedbackItem"
+import { useState } from "react"
 import Header from "./components/Header"
-
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from "./data/FeedbackData"
 function App()
 {
-
+    const[feedback,setFeedback] = useState(FeedbackData)
 
     return(
         <>
         {/* //// This is how u pass on the component to the manin app for rendering and hencforth u can pass props which are meant to be caught by components */}
         <Header />  
-
-
         
         <div className="container">
-            <FeedbackItem />
+            <FeedbackList feedback={feedback}/>
         </div>
         </>
         
