92c67533d2b9962464038890c4f6a7486dbcac3f
Aditya <adityadhayagude03@gmail.com~>
2023-02-22 14:09:04 +0530

    Adding in Conditional Style And How it Works

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { Children } from 'react'
+import PropTypes from 'prop-types'
 
 function Card({children,reverse}) {
 
-  //This is conditional class and that's how it works
+  //This is conditional style and that's how it works
 
   return (
-    <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+    <div className='card' 
+    style={{
+      backgroundColor:reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+      color: reverse? '#fff' : '#000',
+    }}>
+      {children}
+    </div>
   )
 } 
+
+Card.defaultProps = 
+{
+  reverse:false,
+}
+
+// Crad.PropTypes = {
+//   children:PropTypes.node.isRequired,
+//   reverse:Prop
+// }
 
 export default Card  
