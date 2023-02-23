68d886f9f0575d00cdbf3ebd17c012827d88e16e
Aditya <adityadhayagude03@gmail.com~>
2023-02-23 13:16:54 +0530

    From Input And State

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import Card from "./shared/Card"
+
+
+function FeedbackForm() {
+    // const[text,setText] = useState{''}
+
+    const handleTextChange = (e) => {
+        console.log(e.target.value);
+    }
+
+  return (
+    <Card>
+        <form action="">
+            <h2>How would you like to rate your service with us ?</h2>
+            {/* @todo - rating select componenet */}
+            <div className="input-group">
+                <input onChange={handleTextChange} type="text" placeholder="Write a review" />
+                <button type="submit">Send</button>
+            </div>
+        </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
