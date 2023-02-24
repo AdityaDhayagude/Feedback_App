62e89f969652cdb3325dd32240de8bc826225c71
Aditya <adityadhayagude03@gmail.com~>
2023-02-24 15:19:35 +0530

    Creating React Router Method and learning diff betweem v 5 & v 6 (installing react router dom)

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import Card from "../components/shared/Card"
+function AboutPage() {
+  return (
+    <Card>
+    <div className="about">
+    <h1>About the Project</h1>
+    <p>This is a react app to leave feedback for a product or a service</p>
+    </div>
+    </Card>
+  )
+}
+
+export default AboutPage
