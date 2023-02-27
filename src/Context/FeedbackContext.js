73c4fcdf34c6001c078ad16418771dbcc139ec12
Aditya <adityadhayagude03@gmail.com~>
2023-02-27 15:48:11 +0530

    Edit Feedbak Event

--- src/Context/FeedbackContext.js
+++ src/Context/FeedbackContext.js
@@ -22,15 +22,31 @@ export const FeedbackProvider = ({ children }) => {
     },
   ])
 
+  const [feedbackEdit,setFeedbackEdit] = useState({
+    item:{},
+    edit:false
+  } )
+
+  //Add Feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
   }
 
+//Delete Feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
+  }
+
+  //Set Item to Update
+  const editFeedback = (item) =>
+  {
+    setFeedbackEdit({
+        item,
+        edit:true
+    })
   }
 
   return (
@@ -39,6 +55,7 @@ export const FeedbackProvider = ({ children }) => {
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}
